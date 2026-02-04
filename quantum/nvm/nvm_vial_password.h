ed4992ce592383e25dbae35853eaab912b5a6160
Volodymyr Shcherbyna <scherbina.v@gmail.com>
2026-02-04 19:14:07 +0200

    Document Vial password salt buffer expectations

--- quantum/nvm/nvm_vial_password.h
+++ quantum/nvm/nvm_vial_password.h
@@ -8,5 +8,13 @@
 #    define VIAL_PASSWORD_SALT_SIZE 32
 #endif
 
+/**
+ * @brief Reads the Vial password salt from NVM.
+ * @param salt Buffer for the salt, must be at least VIAL_PASSWORD_SALT_SIZE bytes.
+ */
 void nvm_vial_password_get_salt(uint8_t *salt);
+/**
+ * @brief Writes the Vial password salt to NVM.
+ * @param salt Buffer with the salt, must be at least VIAL_PASSWORD_SALT_SIZE bytes.
+ */
 void nvm_vial_password_set_salt(const uint8_t *salt);
